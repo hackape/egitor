284b384eb907f70d90f13823fead30efc9d129c1
hackape <muse.vincent@gmail.com>
2018-05-23 19:12:50 +0800

    style: let resources register themselves on creation

--- src/sdk/Group.ts
+++ src/sdk/Group.ts
@@ -1,11 +1,15 @@
-import { observable, computed, ObservableMap } from "mobx";
+import { values, observable, computed, action, ObservableMap } from "mobx";
 import Resource from "./Resource";
+import Tab from "@/sdk/Tab";
+import getIdFactory from "@/app/utils/getIdFactory";
+const getId = getIdFactory();
 
 class Group extends Resource {
   resources: ObservableMap<any, Group> = this.$$resources;
   constructor() {
     super("groups");
-    this.id = String(Math.random());
+    this.id = `group@${getId()}`;
+    this.register();
   }
 
   @observable activeTabId: string;
--- src/sdk/Tab.ts
+++ src/sdk/Tab.ts
@@ -1,11 +1,14 @@
-import { ObservableMap } from "mobx";
+import { observable, computed, action, ObservableMap } from "mobx";
 import Resource from "./Resource";
+import getIdFactory from "@/app/utils/getIdFactory";
+const getId = getIdFactory();
 
 class Tab extends Resource {
   resources: ObservableMap<any, Tab> = this.$$resources;
   constructor() {
     super("tabs");
-    this.id = String(Math.random());
+    this.id = `tab@${getId()}`;
+    this.register();
   }
 }
 
